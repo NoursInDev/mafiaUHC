9636f746e87f3942d0e27288cf9dfca7c484a8af
NoursInDev <nours44.mc@gmail.com>
2024-04-22 10:44:55 +0200

    Alpha v0.0.6.1

--- game_plugin/MafiaUHC/src/main/kotlin/commandes/General.kt
+++ game_plugin/MafiaUHC/src/main/kotlin/commandes/General.kt
@@ -8,30 +8,79 @@ import org.bukkit.command.CommandSender
 import org.noursindev.mafiauhc.elements.GamePhase
 import java.io.ObjectInputFilter.Config
 
-class commandRead(private val plugin: MafiaUHC) : CommandExecutor{
+class commandRead(private val plugin: MafiaUHC) : CommandExecutor {
 
-    fun analyseArguments(command: Command, args: Array<out String>, sender: CommandSender){
+    fun analyseArguments(command: Command, args: Array<out String>, sender: CommandSender) {
         when {
             args.isEmpty() || args[0] == "infos" || args[0] == "info" -> {
                 val pluginDt = plugin.description
-                sender.sendMessage(pluginDt.name + " v" + pluginDt.version + " by " + pluginDt.authors )
+                sender.sendMessage(pluginDt.name + " v" + pluginDt.version + " by " + pluginDt.authors)
             }
+
             args[0] == "config" -> {
-                if (sender.isOp) { plugin.phase = ConfigPhase(sender, plugin.server.onlinePlayers.toTypedArray()) }
+                if (sender.isOp) {
+                    plugin.phase = ConfigPhase(sender, plugin.server.onlinePlayers.toTypedArray())
+                }
             }
+
             else -> {
-                if (plugin.phase != null){
-                    when (plugin.phase!!.javaClass){
+                if (plugin.phase != null) {
+                    when (plugin.phase!!.javaClass) {
                         ConfigPhase::class.java -> {
-                            when (args[0]){
+                            val configPhase = plugin.phase as ConfigPhase
+                            when (args[0]) {
                                 "rmplayer" -> {
-                                    val configPhase = plugin.phase as ConfigPhase
                                     if (args.size == 1 || plugin.server.getPlayer(args[1]) == null) {
                                         sender.sendMessage("Joueur introuvable")
                                     } else if (configPhase.boite.playerDansListe(args[1])) {
+                                        configPhase.boite.retirePlayer(plugin.server.getPlayer(args[1]))
                                         sender.sendMessage("Retrait d'un joueur")
                                     } else {
                                         sender.sendMessage("Aucun joueur à retirer")
+                                    }
+                                }
+
+                                "addplayer" -> {
+                                    if (args.size == 1 || plugin.server.getPlayer(args[1]) == null) {
+                                        sender.sendMessage("Joueur introuvable")
+                                    } else if (!configPhase.boite.playerDansListe(args[1])) {
+                                        configPhase.boite.ajoutePlayer(plugin.server.getPlayer(args[1]))
+                                        sender.sendMessage("Ajout d'un joueur")
+                                    } else {
+                                        sender.sendMessage("Joueur déjà présent dans la liste")
+                                    }
+                                }
+
+                                "set" -> {
+                                    if (args.size >= 3) {
+                                        val nbstring = args[2]
+                                        val nb = nbstring.toInt()
+                                        when (args[1]) {
+                                            "pierres" -> {
+                                                configPhase.boite.resetPierres(nb)
+                                                sender.sendMessage("Nombre de pierres mis à " + nbstring)
+                                            }
+
+                                            "fideles" -> {
+                                                configPhase.boite.resetFideles(nb)
+                                                sender.sendMessage("Nombre de fidèles mis à " + nbstring)
+                                            }
+
+                                            "agents" -> {
+                                                configPhase.boite.resetAgents(nb)
+                                                sender.sendMessage("Nombre d'agents mis à " + nbstring)
+                                            }
+
+                                            "chauffeurs" -> {
+                                                configPhase.boite.resetChauffeurs(nb)
+                                                sender.sendMessage("Nombre de chauffeurs mis à " + nbstring)
+                                            }
+
+                                            "nettoyeurs" -> {
+                                                configPhase.boite.resetNettoyeurs(nb)
+                                                sender.sendMessage("Nombre de chauffeurs mis à " + nbstring)
+                                            }
+                                        }
                                     }
                                 }
 
@@ -41,6 +90,7 @@ class commandRead(private val plugin: MafiaUHC) : CommandExecutor{
                                 }
                             }
                         }
+
                         GamePhase::class.java -> {
 
                         }
